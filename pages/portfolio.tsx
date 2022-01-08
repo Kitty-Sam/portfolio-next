fe4e3b7a2fcc52c9435605b2030ca1038842cb1e
kitty-sam <www.moonlight@mail.ru>
2022-01-09 00:00:28 +0300

    add portfolio title

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,10 +1,21 @@
 import MainLayout from "../components/MainLayout";
+import styles from "../styles/Portfolio.module.css";
 
 
 const Portfolio = () => {
     return (
         <MainLayout title={'Portfolio project'}>
-            <h1>Portfolio projects</h1>
+          <div>
+                <h2 className={styles.header}>Portfolio projects</h2>
+           <div className={styles.projectsWrapper}>
+                  <div className={styles.projects}>
+                    <div>todolist</div>
+                    <div>game "Cards"</div>
+                    <div>socialNetwork</div>
+                </div>
+              </div>
+            </div>
+
 
         </MainLayout>
     );
