754fb985f96feca32c7b80f3c6cae9cab3a0321a
kitty-sam <www.moonlight@mail.ru>
2022-01-09 12:41:08 +0300

    fix color preloader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ export default function MyApp({Component, pageProps}) {
     return (
         <>
             <NextNprogress
-                color="yellow"
+                color="#ffc107"
                 startPosition={0.3}
                 stopDelayMs={200}
                 height={3}
