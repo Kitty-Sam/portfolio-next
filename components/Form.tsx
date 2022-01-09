6a1ef2af93453215a1b89d8c701dc1e97c927cc6
kitty-sam <www.moonlight@mail.ru>
2022-01-09 20:54:11 +0300

    refactor form component with emailjs

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import styles from "../styles/Contacts.module.css";
+import emailjs  from '@emailjs/browser';
 
 const Form = () => {
     const [name, setName] = useState('');
@@ -13,24 +14,37 @@ const Form = () => {
             email,
             message,
         };
-        console.log(data);
+
+        emailjs.send('service_ythos8o', 'template_gba88jb', data,
+            'user_pWdoipaC9W6BeadqEPHTq')
+            .then(function(response) {
+                console.log('SUCCESS!', response.status, response.text);
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+
+
+
     };
 
     return (
         <div className={styles.message}>
             <form onSubmit={handleSubmit}>
                 <div className={styles.inputInfo}>
-                    <div><input className={styles.nameSend} placeholder={"your name"}
-                                id="name"
-                                type="text"
-                                onChange={e => setName(e.target.value)}
-                    /></div>
-
-                    <div><input className={styles.mailSend} placeholder={"your mail"}
-                                id="email"
-                                type="email"
-                                onChange={e => setEmail(e.target.value)}
-                    /></div>
+                    <div>
+                        <input className={styles.nameSend} placeholder={"your name"}
+                               id="name"
+                               type="text"
+                               onChange={e => setName(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <input className={styles.mailSend} placeholder={"your mail"}
+                               id="email"
+                               type="email"
+                               onChange={e => setEmail(e.target.value)}
+                        />
+                    </div>
                 </div>
                 <textarea className={styles.textarea} placeholder={"your message"}
                           id="message"
